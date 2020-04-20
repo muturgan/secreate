78198f3838b292f1b4fd757f85b7811d3af38248
Andrey Sakharov <a.sakharov@biletix.ru>
2020-04-20 18:14:06 +0300

    store was implemented

--- src/custom_types/index.ts
+++ src/custom_types/index.ts
@@ -4,3 +4,26 @@ export interface IEntity {
    name: TEntityName;
    value: number;
 }
+
+export interface IStore {
+   Entity1: number;
+   Entity2: number;
+   Entity3: number;
+   Entity4: number;
+   Entity5: number;
+   Entity6: number;
+   Entity7: number;
+   Entity8: number;
+   Entity9: number;
+   Entity10: number;
+   Entity11: number;
+   Entity12: number;
+   Entity13: number;
+   Entity14: number;
+   Entity15: number;
+   Entity16: number;
+   Entity17: number;
+   Entity18: number;
+   Entity19: number;
+   Entity20: number;
+}
--- src/server/listener.ts
+++ src/server/listener.ts
@@ -1,9 +1,10 @@
 import type { IEntity } from '../custom_types';
 import SocketIoClient from 'socket.io-client';
+import { Store } from './store';
 const config = require('../../config/config.json');
 
 const socket = SocketIoClient('http://localhost:' + config.sourcePort);
 
 socket.on('next', (entity: IEntity) => {
-   console.log(entity);
+   Store.updateEntity(entity);
 });
